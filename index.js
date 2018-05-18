529947e5b5a3f6ebc6048c976546417b5bd57cf5
Ramesh, Hariharan (US - Bengaluru) <hariramesh@deloitte.com>
2018-05-18 11:59:43 +0530

    5

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 5000
 //   .set('view engine', 'ejs')
 //   .get('/', (req, res) => res.render('pages/index'))
 //   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-var express = require('express');
-//var bodyParser = require('body-parser');
-var path = require('path');
+const express = require('express');
+var bodyParser = require('body-parser');
+const path = require('path');
 
 var app = express();
-// var apiai = require('apiai');
-// var apiapp = apiai("d92b6db3d22745a78f064f96fce19db4");
+var apiai = require('apiai');
+var apiapp = apiai("d92b6db3d22745a78f064f96fce19db4");
 
 //Body parser
 //app.use(bodyParser.json());
@@ -24,23 +24,23 @@ app.get('/', function(req, res){
   res.send('Hello Salesforce');
 });
 
-// app.post('/chat', function(req, res){
-//
-//   var request = apiapp.textRequest(req.body.chatText, {
-//     sessionId: '4BGUAgBUD_1NO0WOl8UjukTbKa4C5FdnYYO25tlVNknms6medUzZrGHXsC3FH72'
-//   });
-//   request.on('response', function(response) {
-//       console.log(response);
-//       res.send(response);
-//   });
-//   request.on('error', function(error) {
-//       console.log(error);
-//
-//       res.send(error);
-//   });
-//   request.end();
-// });
+app.post('/chat', function(req, res){
 
-app.listen(port, function(){
+  var request = apiapp.textRequest(req.body.chatText, {
+    sessionId: '4BGUAgBUD_1NO0WOl8UjukTbKa4C5FdnYYO25tlVNknms6medUzZrGHXsC3FH72'
+  });
+  request.on('response', function(response) {
+      console.log(response);
+      res.send(response);
+  });
+  request.on('error', function(error) {
+      console.log(error);
+
+      res.send(error);
+  });
+  request.end();
+});
+
+app.listen(PORT, function(){
   console.log('server started on Port 3000...')
 });
